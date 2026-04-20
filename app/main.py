688cc7bf2e7946dc399608affed3aa992aff2271
Rywin Patcharaput <rywinp@umich.edu>
2026-04-20 10:53:02 -0400

    Reformat main.py for flake8

--- app/main.py
+++ app/main.py
@@ -2,25 +2,33 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import boto3
 
+
 # Pydantic Models for validating request bodies
-# Will reject requests that don't match this request body with Status 422 Unprocessable Entity
-# If we want strict strings, pydantic offers StrictStr type, because right now the client can send numbers for the content and filename fields
-# and Pydantic will convert the number to a string, thus validating the request body
+# Will reject requests that don't match this request body
+# with Status 422 Unprocessable Entity
+# If we want strict strings, pydantic offers StrictStr type, because right
+# now the client can send numbers for the content and filename fields
+# and Pydantic will convert the number to a string, thus validating
+# the request body
 class ItemRequest(BaseModel):
     filename: str
     content: str
+
 
 # The response for the /upload endpoint
 class ItemResponse(BaseModel):
     uploaded: bool
     filename: str
 
+
 app = FastAPI()
-client = boto3.client('s3')
+client = boto3.client("s3")
+
 
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
 
 @app.post("/upload")
 async def upload(item: ItemRequest) -> ItemResponse:
@@ -35,7 +43,8 @@ async def upload(item: ItemRequest) -> ItemResponse:
     #   - convert all contents to plain text
     #   - accept any filename
 
-    # FastAPI will reject requests that don't match the ItemRequest Pydantic Model
+    # FastAPI will reject requests that
+    # don't match the ItemRequest Pydantic Model
 
     MAX_CONTENT_SIZE = 1_000_000
     BUCKET_NAME = "secops-assignment-rywin"
@@ -51,9 +60,7 @@ async def upload(item: ItemRequest) -> ItemResponse:
             Key=item.filename,
             Body=item.content.encode("utf-8"),
             ContentType="text/plain",
-            Metadata={
-                "filename": item.filename
-            }
+            Metadata={"filename": item.filename},
         )
     except Exception as e:
         # Error from AWS
